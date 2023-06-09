b95c458df4af4b849519b8de36a2984269a852b7
João Francisco Rodrigues <joao.rodrigues@viasoft.com.br>
2023-06-09 11:14:07 -0300

    refactor: refatoração de código

--- app/microphone.js
+++ app/microphone.js
@@ -2,42 +2,46 @@ class Microphone {
   constructor() {
     this.initialized = false;
     //coleta dados do microfone
-    navigator.mediaDevices.getUserMedia({audio: true})
-    .then(function(stream) {
-      //converte para o formato adequado
-      this.audioContext = new AudioContext();
-      this.microphone = this.audioContext.createMediaStreamSource(stream);
-      //cria e configura analyser
-      this.analyser = this.audioContext.createAnalyser();
-      this.analyser.fftSize = 2048;
-      
-      //converte para 8bit audio
-      const bufferLength = this.analyser.frequencyBinCount;
-      this.dataArray = new Uint8Array(bufferLength);
+    navigator.mediaDevices
+      .getUserMedia({ audio: true })
+      .then(
+        function (stream) {
+          //converte para o formato adequado
+          this.audioContext = new AudioContext();
+          this.microphone = this.audioContext.createMediaStreamSource(stream);
+          //cria e configura analyser
+          this.analyser = this.audioContext.createAnalyser();
+          this.analyser.fftSize = 2048;
 
-      //conecta o microfone ao analyser
-      this.microphone.connect(this.analyser);
-      
-      this.initialized = true;
-      //bing serve para que o then entenda o this
-    }.bind(this)).catch(err => alert(err));
+          //converte para 8bit audio
+          const bufferLength = this.analyser.frequencyBinCount;
+          this.dataArray = new Uint8Array(bufferLength);
+
+          //conecta o microfone ao analyser
+          this.microphone.connect(this.analyser);
+
+          this.initialized = true;
+          //bing serve para que o then entenda o this
+        }.bind(this)
+      )
+      .catch((err) => alert(err));
   }
 
   // retorna array para o visualizer visualizar
-  getSamples(){
+  getSamples() {
     this.analyser.getByteTimeDomainData(this.dataArray);
 
     // normaliza os dados do sample para um range de -1 e 1
-    let normSamples = [...this.dataArray].map(e => e/256 - 1);
+    let normSamples = [...this.dataArray].map((e) => e / 256 - 1);
     return normSamples;
   }
   // retorna o volume do microfone
-  getVolume(){
+  getVolume() {
     this.analyser.getByteTimeDomainData(this.dataArray);
-    let normSamples = [...this.dataArray].map(e => e/256 - 1);
+    let normSamples = [...this.dataArray].map((e) => e / 256 - 1);
     let sum = 0;
     // RMS -> Root Means Square é uma medida de magnitude entre um grupo de números
-    for(let i = 0; i < normSamples.length; i++) {
+    for (let i = 0; i < normSamples.length; i++) {
       // os negativos serão transformados em positivos
       sum += normSamples[i] * normSamples[i];
     }
--- app/visualizer.js
+++ app/visualizer.js
@@ -1,67 +1,61 @@
-function main() {
-  // configura o canvas do html
-  const canvas = document.getElementById('myCanvas');
-  const ctx = canvas.getContext('2d');
+const main = () => {
+  const canvas = document.getElementById("myCanvas");
+  const ctx = canvas.getContext("2d");
   canvas.width = window.innerWidth;
   canvas.height = window.innerHeight;
 
-  // essa classe, quando call, irá gerar cada uma das barras que irão interagir com o microfone
+  // essa classe, quando chamada, irá gerar cada uma das barras que irão interagir com o microfone
   class Bar {
-    constructor(x, y, width, height, color, index) {
+    constructor(x, y, width, height, color) {
       this.x = x;
       this.y = y;
       this.width = width;
       this.height = height;
       this.color = color;
-      this.index = index;
-    };
+    }
     update(micInput) {
-      this.height = micInput * 100;
-    };
+      this.height = micInput * 500;
+    }
     draw(context) {
       context.strokeStyle = this.color;
       context.save();
 
-      context.translate(canvas.width/2,canvas.height/2);
-      context.rotate(this.index++ * 2);
-
+      context.translate(this.x, this.y);
       context.beginPath();
-      context.moveTo(500, this.height);
-      context.lineTo(0.5, this.y);
+      context.moveTo(this.width, this.height);
+      context.lineTo(this.x, this.y);
       context.stroke();
 
       context.restore();
-    };
+    }
   }
-
-  // renderizando as barras
   const microphone = new Microphone();
   let bars = [];
-  let barWidth = canvas.width/248;
-  let barHeight = canvas.height/248;
+  let barWidth = canvas.width / 256;
+  let barHeight = canvas.height / 2;
 
-  function createBars() {
-    for(let i = 0; i < 2048; i++) {
-      let color = 'hsl(' + i * 2 + ', 100%, 50%)';
-      bars.push(new Bar(i * barWidth, i, 100, 100, color, i));
-    };
-  }
+  const createBars = () => {
+    for (let i = 0; i < 256; i++) {
+      let color = "hsl(" + i * 3 + ", 100%, 50%)";
+      bars.push(new Bar(i * barWidth, barHeight, 0.8, 20, color));
+    }
+  };
   createBars();
 
-  function animate() {
-    if(microphone.initialized) {
-      // ctx.clearRect(0, 0, canvas.width, canvas.height);
+  const animate = () => {
+    if (microphone.initialized) {
+      ctx.clearRect(0, 0, canvas.width, canvas.height);
       // gerar sample de audio que vem do microfone
       const samples = microphone.getSamples();
-  
-      // faz a animação em loop
+
+      // animação em loop
       bars.forEach((bar, i) => {
         bar.update(samples[i]);
         bar.draw(ctx);
-      })
+      });
     }
     requestAnimationFrame(animate);
-  }
+  };
 
   animate();
-}
+};
